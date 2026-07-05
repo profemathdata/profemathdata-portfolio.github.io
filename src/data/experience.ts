3f97bd7c2a8f677887f8ab7da83a4467d7369726
profemathdata <profe.math.data@gmail.com>
2026-07-05 07:38:24 -0400

    Update experience details for data analyst role

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -18,12 +18,13 @@ export const experience = [
     ],
   },
   {
-    company: 'Web Solutions Co.',
-    position: 'Junior Software Developer',
-    duration: 'January 2015 - May 2017',
+    company: 'Carabineros de Chile.',
+    position: 'Analista de procesamiento de datos (Data Analyst)',
+    duration: 'Dic 2018 - sep 2023',
     responsibilities: [
-      'Assisted in the development of web applications and performed bug fixes.',
-      'Contributed to the documentation of code ',
+      'Modelamiento estadístico y predictivo de delitos; creación de dashboards de KPIs institucionales.',
+      'Diseño de planes de acción basados en datos para optimización de recursos policiales.',
+      'Logro: Implementé una estructura de Análisis y modelamiento estadístico de Delitos lo que permitió desarrollar y ejecutar planes de acción que redujeron la tasa de victimización hasta un 13% en 2 años.',
     ],
   }
 ]

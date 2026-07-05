f9815c8a7f78b88bea37550f5bf8cb7e20550472
profemathdata <profe.math.data@gmail.com>
2026-07-04 22:14:06 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig} from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   side: 'https://profemathdata-portfolio.github.io',
+  base: '/profemathdata-portfolio.github.io'
 });

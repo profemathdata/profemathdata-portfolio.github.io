1d5b6c4c6b20b788f04b4f6a1d3e05da71bbe3bb
profemathdata <profe.math.data@gmail.com>
2026-07-09 21:59:24 -0400

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -40,7 +40,7 @@ export const experience = [
     position: 'Técnico de Mantenimiento',
     duration: 'Abr 2015 - Abr 2016',
     responsibilities: [
-      ' Instrumentista en parada de planta, calibración de válvulas de presión con actuador neumático y posicionador marca Samson, mantenimiento de rotámetros y reguladores de Presión. Reposición de emisores en mal estado para etapa de Solaronics, remoción de material sobrante en límites de carrera y correa transportadora para etapa de FIBRON. calibración de sensores de nivel, desarme de válvulas solenoide marca Danfoss y verificación de estado ',
+      ' Instrumentista en parada de planta, calibración de válvulas de presión con actuador neumático y posicionador, reposición de emisores, calibración de sensores de nivel',
     ],
   },
   {
@@ -48,7 +48,7 @@ export const experience = [
     position: 'Técnico Instrumentista y Técnico de Dibujo AUTOCAD ',
     duration: 'Sep 2011 - feb 2012',
     responsibilities: [
-      'Técnico planta Termoeléctrica Santa María Coronel, calibrando válvulas neumáticas marca Siemens con posicionador marca Festo, transmisores nivel, de presión, de temperatura Marca Endress+Hauser, Cableado para sistema de control del Water Canon. Ademas  corrección de Planos Eléctricos de Calderas, Edificios Eléctricos, Turbine Hall, Área de desalinización para Ingeniería de Detalle.',
+      'Técnico planta Termoeléctrica Santa María Coronel, calibrando válvulas neumáticas y posicionadores, transmisores nivel, de presión, de temperatura, Cableado para sistema de control. Ademas  corrección de Planos Eléctricos para Ingeniería de Detalle.',
     ],
   },
     {
